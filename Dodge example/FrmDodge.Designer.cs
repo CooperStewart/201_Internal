b14fa351fc5eccab4772c3910386c521e6384c25
Cooper Stewart <cooperstewart@mmc.school.nz>
2020-07-21 14:33:58 +1200

    fuck fuck fuck

---
+++ Dodge example/FrmDodge.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Dodge_example
+{
+    partial class FrmDodge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.TmrPlanet = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.Location = new System.Drawing.Point(-3, 5);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(635, 457);
+            this.PnlGame.TabIndex = 1;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
+            // 
+            // TmrPlanet
+            // 
+            this.TmrPlanet.Enabled = true;
+            this.TmrPlanet.Tick += new System.EventHandler(this.TmrPlanet_Tick);
+            // 
+            // FrmDodge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 461);
+            this.Controls.Add(this.PnlGame);
+            this.Name = "FrmDodge";
+            this.Text = "Dodge";
+            this.Load += new System.EventHandler(this.FrmDodge_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.Timer TmrPlanet;
+    }
+}
+
